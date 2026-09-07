feaf189ba4e6fba38199776e7dbb066e2b8d4e95
WonkaKoushi <wonkasensei@gmail.com>
2026-09-08 01:34:18 +0900

    a_01

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+ 
+public class ScoreManager : MonoBehaviour
+{
+    private int score = 100;
+ 
+    public void AddPoints(int points)
+    {
+        score += points;
+    }
+ 
+    public int GetScoreValue()
+    {
+        return score;
+    }
+}
+ 
