e501123795bd5176081c89c6891161ab25fdb876
WonkaKoushi <wonkasensei@gmail.com>
2026-09-09 19:57:27 +0900

    a/02修正

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
-
+ 
 public class PlayerJump : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
-
+ 
     private int jumpsUsed = 0;
     private int maxJumps = 2;
-
+ 
     public void Jump()
     {
         if (jumpsUsed < maxJumps)
@@ -15,7 +15,7 @@ public class PlayerJump : MonoBehaviour
             rb.AddForce(Vector2.up * 5f, ForceMode2D.Impulse);
         }
     }
-
+ 
     public void Land()
     {
         jumpsUsed = 0;
